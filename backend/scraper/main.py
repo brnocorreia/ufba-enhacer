a2910ea15d2137f903aec01c1bf86d8753d8b542
Bruno Correia <bruninhocorreia2013@gmail.com>
2023-12-20 22:35:29 -0300

    fix: exploit error fixed

--- backend/scraper/main.py
+++ backend/scraper/main.py
@@ -10,10 +10,10 @@ def main():
     
     driver.get(login.URL)
     cpf = driver.find_element(login.CPF, 10, EC.element_to_be_clickable)
-    cpf.send_keys('86380840567')
+    cpf.send_keys('CPF AQUI')
 
     password = driver.find_element(login.PASS, 10, EC.element_to_be_clickable)
-    password.send_keys('bruno030300')
+    password.send_keys('SENHA AQUI')
 
     driver.find_element(login.LOGIN_BUTTON, 10, EC.element_to_be_clickable).click()
 
