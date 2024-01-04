1186fa0aa2b7f924d9a9ac66a856c95c6fa50b71
Bruno Correia <bruninhocorreia2013@gmail.com>
2024-01-04 20:46:44 -0300

    feat: package structures

--- backend/api/cmd/main.go
+++ backend/api/cmd/main.go
@@ -3,12 +3,37 @@ package main
 import (
 	"log"
 
+	"github.com/brnocorreia/ufba-enhacer/internal/configs/database"
+	"github.com/brnocorreia/ufba-enhacer/internal/model/repository/entity"
 	"github.com/joho/godotenv"
 )
+
+var models = []interface{}{&entity.Course{}, &entity.Discipline{}, &entity.Prerequisite{}, &entity.CoursesDiscipline{}}
 
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	db, err := database.NewDBConnection()
+	if err != nil {
+		log.Fatal("Error connecting to db")
+	}
+
+	db.AutoMigrate(models...)
+
+	// new_course := entity.Course{
+	// 	Code:        3160,
+	// 	Name:        "ADMINISTRAÇÃO",
+	// 	Shift:       "Diurno",
+	// 	MinDuration: 4.0,
+	// 	MaxDuration: 8.0,
+	// 	LegalBase:   "DATA DE INÍCIO: 03.03.1961. RECONHECIMENTO: PARECER CFE Nº 167 DE 27.12.1962. DIRETRIZES CURRICULARES: PARECER CNE Nº 04 de 13.07.2005",
+	// 	Description: "Ao Administrador compete o plajejamento e a implantação de estruturas ognizacionais, dos métodos de trabalho e da utilização dos recursos humanos, materiais e financeiros disponíveis, com vistas à consecução dos objetivos da organicação (governamental ou privada) a que serve. Para isso, lança mão e desenvolve técnicas de administração de pessoal, de liderança e treinamento, de interrelacionamento de pessoas e grupos, de administração financeira e contábil, programação, orçamentação, pesquisa, processamento de dados, controle de produção, estudo de mercado, vendas, etc.",
+	// 	ObWorkload:  2805,
+	// 	OpWorkload:  240,
+	// 	AcWorkload:  100,
+	// 	Disciplines: []entity.Discipline{},
+	// }
 }
